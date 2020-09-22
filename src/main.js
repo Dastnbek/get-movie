9bb6927fee2bf5391fe78de301c4959321076e3b
dastnbek <dostonbek.oripjonov@mail.ru>
2020-09-22 21:02:52 +0500

    Bootstrap installed

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
 import Vue from 'vue'
-import App from './App.vue'
+// bootstrap
+import { BootstrapVue } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+//pwa
 import './registerServiceWorker'
+// vue-router
 import router from './router'
+// vuex
 import store from './store'
+//custom
+import App from './App.vue'
 
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
 
 new Vue({
   router,

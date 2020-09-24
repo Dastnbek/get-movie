6a9f1fbdee1763cd12ca9b2ea50f94e988ffa7cd
dastnbek <dostonbek.oripjonov@mail.ru>
2020-09-24 23:01:29 +0500

    Movies blueprint is ready

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -1,13 +1,32 @@
 <template>
     <b-card :img-src="'https://image.tmdb.org/t/p/w300/' + movie.poster_path" img-alt="Card image" img-left class="mb-3">
-        <b-card-text>
-            {{ movie.original_title }}
-        </b-card-text>
+        <div :class="[titleContainer]" >
+            <span :class="[movieTitle]" >
+                {{ movie.title }}
+            </span>
+        </div>
+        <div>
+            <div :class="infoContainer" >
+                <div>
+                    Points : {{movie.vote_average}}/10
+                </div>
+                <div>
+                    Release date : {{movie.release_date}}
+                </div>
+            </div>
+        </div>
     </b-card>
 </template>
 
 <script>
 export default {
+    data(){
+        return {
+            movieTitle : "title",
+            titleContainer : "titleContainer",
+            infoContainer : 'infoContainer'
+        }
+    },
     props : ['movie']
 }
 </script>
@@ -15,5 +34,27 @@ export default {
 <style scoped>
 .card-img-left{
     height: 150px;
+}
+
+.card-body{
+    padding: 5px;
+}
+
+.titleContainer{
+    height: 60px;
+}
+
+.title{
+    font-size: 11px;
+    font-weight: bold;
+}
+.infoContainer{
+    font-size: 11px;
+    position: absolute;
+    bottom: 0;
+
+}
+.card-text {
+    font-size: 10px;
 }
 </style>
--- src/views/Movies.vue
+++ src/views/Movies.vue
@@ -1,6 +1,24 @@
 <template>
     <b-container >
-        <h2> Top Rated </h2>
+        <div :class="[movieTypeCon]" > 
+          <span :class="[movieType]">New</span>     
+        </div>
+        <b-row id="myRow" >
+            <b-col cols="3" v-for="movie in upComing">
+                <MovieCard :movie='movie' />
+            </b-col>
+        </b-row>
+        <div :class="[movieTypeCon]" > 
+          <span :class="[movieType]">Popular</span>     
+        </div>
+        <b-row id="myRow" >
+            <b-col cols="3" v-for="movie in popular">
+                <MovieCard :movie='movie' />
+            </b-col>
+        </b-row>
+        <div :class="[movieTypeCon]" > 
+          <span :class="[movieType]">Top Rated</span>     
+        </div>
         <b-row id="myRow" >
             <b-col cols="3" v-for="movie in topRated">
                 <MovieCard :movie='movie' />
@@ -16,13 +34,26 @@ import MovieCard from '../components/MovieCard'
 export default {
     data(){
         return { 
-            topRated : []
+            upComing : [],
+            popular : [],
+            topRated : [],
+            movieType : "movieType",
+            movieTypeCon : "movieTypeCon"
         }
     },
     components : {
         MovieCard
     },
     mounted() {
+        //up coming
+        axios
+        .get("https://api.themoviedb.org/3/movie/upcoming?api_key=06aa50e38281dd9b38543df33f8bab2c&language=en-US&page=1")
+        .then(response => this.upComing = response.data.results)
+        //popular
+        axios
+        .get("https://api.themoviedb.org/3/movie/popular?api_key=06aa50e38281dd9b38543df33f8bab2c&language=en-US&page=1")
+        .then(response => this.popular = response.data.results)
+        //top rated
         axios
         .get('https://api.themoviedb.org/3/movie/top_rated?api_key=06aa50e38281dd9b38543df33f8bab2c&language=en-US&page=1')
         .then(respons => this.topRated = respons.data.results)
@@ -34,8 +65,19 @@ export default {
 #myRow{
     width:100% ;
     overflow-x: scroll ;
+    margin-bottom: 25px;
 }
-
+.movieTypeCon{
+    margin : 10px 0;
+}
+.movieType{
+    font-size: 25px;
+    border: 1px solid;
+    border-radius: 2px;
+    box-shadow: 5px 5px lightblue;
+    cursor: pointer;
+    padding: 5px;
+}
 .row{
     flex-wrap: nowrap;
 }
